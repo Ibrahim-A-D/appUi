97da36b3fb52a4335d95dd9a7d4c7ea0e69852ca
Ibrahima Assiatou Diallo <IbrahimaAssiatou.Diallo2@orange-sonatel.com>
2023-08-21 09:57:16 +0000

    base url changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/appUi/',
   experimental: {
     renderBuiltUrl(filename: string, { hostId, hostType, type }: { hostId: string, hostType: 'js' | 'css' | 'html', type: 'public' | 'asset' }) {
       if (type === 'public') {
-        return '/src/' + filename
+        return '/appUi/' + filename
       }
       else {
-        return '/src/' + filename
+        return '/appUi/' + filename
       }
     }
   }
